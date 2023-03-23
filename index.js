f3bd30a742318165df20987eeb87c7fca717a152
FeNe <neonfelixe@gmail.com>
2023-03-23 13:02:02 +0800

    Patched v1

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-
-const menuIcon = document.getElementById('menu-icon');
-const menuItems = document.querySelector('.nav-wrapper ul');
-
-menuIcon.addEventListener('click', () => {
-    menuItems.classList.toggle('show');
-});
+<nav class="menubar">
+            <ul>
+                <li><a style="font-weight: bold; border-bottom: 3px solid white; border-radius: 2px;"
+                        href="#Home">Home</a>
+                </li>
+                <li><a href="https://neonfelixe.github.io/SABS-Website-Revamp.github.io/Schedule.html">Schedule</a></li>
+                <li><a href="https://neonfelixe.github.io/SABS-Website-Revamp.github.io/Gallery.html">Gallery</a></li>
+                <li><a href="#contact-info">Contact</a></li>
+                <li><a href="https://github.com/NeonFelixe/SABS-Website-Revamp.github.io" target="_blank">About us</a>
+                </li>
+            </ul>
+        </nav>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+        <script>
+            function myFunction() {
+                var x = document.getElementById("menubar");
+                if (x.style.display === "block") {
+                    x.style.display = "none";
+                } else {
+                    x.style.display = "block";
+                }
+            }
+        </script>
