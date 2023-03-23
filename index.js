12b113ebe351b7bff526cbb99770b3df94044cf9
FeNe <neonfelixe@gmail.com>
2023-03-23 20:31:38 +0800

    Patched v2

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
-<nav class="menubar">
-            <ul>
-                <li><a style="font-weight: bold; border-bottom: 3px solid white; border-radius: 2px;"
-                        href="#Home">Home</a>
-                </li>
-                <li><a href="https://neonfelixe.github.io/SABS-Website-Revamp.github.io/Schedule.html">Schedule</a></li>
-                <li><a href="https://neonfelixe.github.io/SABS-Website-Revamp.github.io/Gallery.html">Gallery</a></li>
-                <li><a href="#contact-info">Contact</a></li>
-                <li><a href="https://github.com/NeonFelixe/SABS-Website-Revamp.github.io" target="_blank">About us</a>
-                </li>
-            </ul>
-        </nav>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-            <i class="fa fa-bars"></i>
-        </a>
-        <script>
-            function myFunction() {
-                var x = document.getElementById("menubar");
-                if (x.style.display === "block") {
-                    x.style.display = "none";
-                } else {
-                    x.style.display = "block";
-                }
-            }
-        </script>
+ndow.addEventListener("scroll", function () {
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("scrolling-active", window.scrollY > 0);
+});
