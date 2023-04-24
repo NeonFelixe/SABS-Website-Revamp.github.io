76cad0752c168eeb2af20b64a194f0366d786c1b
Fe Ne <101835293+NeonFelixe@users.noreply.github.com>
2023-04-24 19:53:10 +0800

    patched v2

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+
+// Hide the loading animation when the page has finished loading
+window.addEventListener('load', function() {
+    document.getElementById('loader').style.display = 'none';
+});
+
+// for floating button (vision and mission)
 const floatingButton = document.querySelector('.floating-button');
 floatingButton.addEventListener('click', () => {
     floatingButton.classList.toggle('floating-window');
@@ -27,21 +34,53 @@ window.addEventListener('load', () => {
 
 
 
-if (window.innerWidth < 1080) {
-    var popup = document.getElementById("popup");
-    popup.style.display = "block";
-}
-
-function closePopup() {
-    var popup = document.getElementById("popup");
-    popup.style.display = "none";
-}
-
-
 var navLinks = document.getElementById("navLinks");
 function showMenu() {
     navLinks.style.right = "0px";
 }
 function hideMenu() {
     navLinks.style.right = "-210px";
+}
+
+// popup for code darkroom
+var popup = document.getElementById("popup");
+
+function showPopup() {
+    popup.style.display = "block";
+}
+
+function closePopup() {
+    popup.style.display = "none";
+}
+
+function checkAccessCode() {
+    var accessCode = document.getElementById("access-code").value;
+    if (accessCode === "1120252") { // replace "myaccesscode" with your actual access code
+        window.open("https://neonfelixe.github.io/SABS-Website-Revamp.github.io/answerkey.html", "_blank")
+        popup.click();
+    } else {
+        alert("Invalid access code. Please try again.");
+    }
+}
+// Detect if the user is on a mobile device
+function isMobile() {
+    return /Mobi/.test(navigator.userAgent);
+}
+
+
+// Open the link in the browser on mobile devices
+if (isMobile()) {
+    popupLink.setAttribute("href", "https://neonfelixe.github.io/SABS-Website-Revamp.github.io/answerkey.html");
+}
+
+// make the navbar hide when scrolled down
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navigation bar").style.top = "0";
+    } else {
+        document.getElementById("navigation bar").style.top = "-70px";
+    }
+    prevScrollpos = currentScrollPos;
 }
